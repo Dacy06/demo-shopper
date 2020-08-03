fe75ee655b6a9063e50fe7b4c067fb688e0c2f53
zhangwenjie16 <zhangwenjie16@meituan.com>
2020-08-03 17:08:31 +0800

    Updates

---
+++ precache-manifest.79a9c9f83bd1329b5487ea7f59d0e14c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d948f141db3e046e2dd0af4dd58ff8c",
+    "url": "/demo-shopper/index.html"
+  },
+  {
+    "revision": "1180b6f753ec768d5c81",
+    "url": "/demo-shopper/static/css/main.e1a2ba3e.chunk.css"
+  },
+  {
+    "revision": "71f536855bfa25bd0d0a",
+    "url": "/demo-shopper/static/js/2.bc82bed8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo-shopper/static/js/2.bc82bed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1180b6f753ec768d5c81",
+    "url": "/demo-shopper/static/js/main.73ae98b8.chunk.js"
+  },
+  {
+    "revision": "c6f7cb7c337e30e09620",
+    "url": "/demo-shopper/static/js/runtime-main.3c7f1122.js"
+  }
+]);
